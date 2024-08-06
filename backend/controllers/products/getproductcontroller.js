d4be803aa567737bd0aaa2e7f906bf0c31fa320d
ankit kumar rai <ankitkumarrai1702@gmail.com>
2024-08-06 12:41:30 +0530

    Continue programming

--- backend/controllers/products/getproductcontroller.js
+++ backend/controllers/products/getproductcontroller.js
@@ -1,8 +1,17 @@
-const getproductcontroller = (req,res)=>{
-    res.send(`
-        <h1> Get Product Controller <h1>
-        `)
-    console.log("Get controller page")
+const mongoose = require("mongoose")
+const ProductCollection = require("../../models/ProductSchema")
+const getproductcontroller = async(req,res)=>{
+    try{
+        const product = await ProductCollection.find()
+        res.status(200).send(product)
+        console.log("product fetched successfully")
+    }
+    catch(error){
+        res.status(500).send({
+            message:"Error in fetching product"
+        })
+        console.log(`error occured : ${error}`)
+    }       
 }
 
 module.exports = getproductcontroller
